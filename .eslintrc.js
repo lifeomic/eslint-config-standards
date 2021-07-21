807d03b2044267d58fa9c7eb437f741fa27aedd4
David Tanner <david.tanner@lifeomic.com>
2021-07-21 07:28:09 -0600

    fix(plugins): Need to enable the jest plugin. Move some standard rules to the javascript section

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,11 @@ module.exports = {
   extends: [
     'eslint',
     'plugin:eslint-plugin/recommended',
-    'plugin:eslint-plugin-node/recommended'
+    'plugin:eslint-plugin-node/recommended',
+    './lib/javascript'
   ],
   plugins: [
     'eslint-plugin',
     'eslint-plugin-node'
-  ],
-  rules: {
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
-    strict: 'off',
-    semi: ['error', 'never']
-  }
+  ]
 }
